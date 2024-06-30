aab0337cb7484cf2f09071959188e38f7eecb540
zilohnoji <donatoordep@gmail.com>
2024-06-30 12:58:29 -0300

    projetos

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-let button = document.querySelector("#btn-mobile");
+// Menu hamburguer
+const button = document.querySelector("#btn-mobile");
 
 button.addEventListener("click", () => {
   document.querySelector("#header-container-nav").classList.toggle("active");
 });
+
+// Menu Tab
+const tabs = document.querySelectorAll(".tab-btn");
+
+tabs.forEach((tab) => tab.addEventListener("click", () => tabClicked(tab)));
+
+const tabClicked = (tab) => {
+  tabs.forEach((tab) => tab.classList.remove("tab-active"));
+  tab.classList.add("tab-active");
+
+  const contents = document.querySelectorAll(".content");
+  contents.forEach((content) => content.classList.remove("show"));
+
+  const contentId = tab.getAttribute("content-id");
+  const content = document.getElementById(contentId);
+
+  content.classList.add("show");
+};
